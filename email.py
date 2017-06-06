e6c1156ae0ee6a25a9e5b1424b4d8227de7700ec
rogerio79 <rogerio.carignano@gmail.com>
2017-06-06 11:51:47 -0300

    Alerta de backup via email

---
+++ email.py
@@ -0,0 +1,68 @@
+import os
+import csv
+import datetime
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
+def abre_arquivo(arquivo) :
+    aux_z = {}
+    with open(arquivo) as csvarquivo:
+        dataset = csv.reader(csvarquivo, delimiter='=')
+        for row in dataset:
+            aux_z[row[0]] = row[1]
+        return aux_z
+
+		
+def envia_email():
+	conf = abre_arquivo('conf_email.csv')
+
+	msg = MIMEMultipart()
+	msg['From'] = conf['from']
+	msg['To'] = conf['to']
+	msg['Subject'] = conf['subject']
+	message = conf['message']
+	msg.attach(MIMEText(message))
+
+	mailserver = smtplib.SMTP_SSL(conf['smtp_ssl'])
+
+	# se identificando para o server
+	mailserver.ehlo()
+
+	mailserver.ehlo()
+
+	mailserver.login(conf['login'], conf['password'])
+	mailserver.sendmail(conf['from'],conf['to'],msg.as_string())
+
+	mailserver.quit()
+	
+	
+def getKey(item):
+	return item[1]
+	
+	
+def verifica_arquivo():	
+	conf = abre_arquivo('conf_path.csv')
+	
+	mypath = conf['path']
+	f = []
+	z = []
+
+	for (dirpath, dirnames, filenames) in os.walk(mypath):
+		f.extend(filenames)
+
+	
+	for arquivo in f:
+		data = os.stat(arquivo).st_ctime
+		tamanho = os.stat(arquivo).st_size
+		z.append([arquivo, data, tamanho])
+
+		
+	z_ordenado = sorted(z, key=getKey, reverse=True) 	
+
+	ultimo_criado, data_criado, tam_criado = z_ordenado[0]
+
+	print(ultimo_criado, str(datetime.datetime.fromtimestamp(data_criado)), float(tam_criado))
+
+envia_email()
+
